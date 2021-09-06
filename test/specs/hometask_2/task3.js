30b33604e6c5c80fb0e7c7646e16df6ef2a54b7a
Uliana <upimenova@exadel.com>
2021-09-06 15:43:13 +0300

    task3

---
+++ test/specs/hometask_2/task3.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+fs.readFile('/etc/passwd', (err, data) => { 
+    if (err) throw err; 
+    const objectToString = JSON.stringify();
+    if (typeof data.flag !== 'boolean') {
+        fs.writeFile('errors.txt', 'flag value: ' + data.flag + ' is not a boolean value', function (err) {
+            if (err) throw err;
+          });
+    }
+    if (!data.myPromises.isArray()) {
+        fs.writeFile('errors.txt', 'myPromises value: ' + data.myPromises + ' is not an array', function (err) {
+            if (err) throw err;
+          });
+    }
+    if (typeof data.element !== 'object' || data.element == null) {
+        fs.writeFile('errors.txt', 'element value: ' + data.element + ' is not an object', function (err) {
+            if (err) throw err;
+          });
+    }
+    console.log(data); 
+  });
+  
